874d3a725a976db5b8a5f699c182283437f9adc1
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-07-20 09:37:37 +0100

    Refactored function into common class

--- combined.py
+++ combined.py
@@ -55,3 +55,20 @@ class JoinGraph:
                 unlink.append(merge)
 
         return json.dumps({"original":original,"link":link,"difference":self.deduplicate_data(unlink,link),"similarity":self.calculate_similarity_perc(link,original)})
+
+    def search_data(self, term):
+        '''
+           Perform substring search on data and return JSON
+        '''
+        original = []
+        sd = SparqlDao()
+
+        qry_string = FileOps().open('query/search_string.rq')
+        qry_string = qry_string.format(term)
+        original = sd.autocomplete_sparql(self.endpoint, qry_string)
+
+        terms  = []
+        for data in original:
+            terms.append({'id': data[0], 'values': data[1]})
+
+        return json.dumps({terms})
