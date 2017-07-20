afbc22e192226e17c17cbf0471cb5cacd91c1918
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-07-20 09:34:35 +0100

    Added some documentation to sparql functions

--- sparqldao.py
+++ sparqldao.py
@@ -4,7 +4,13 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 class SparqlDao:
+    '''
+       class contains wrapper methods for the Sparql queries
+    '''
     def run_remote_sparql(self,endpoint, query):
+        '''
+           Method to run remote Sparql queries
+        '''
         data = []
 
         sparql = SPARQLWrapper(endpoint)
@@ -14,5 +20,21 @@ class SparqlDao:
         results = sparql.query().convert()
 
         data = [(r["s"]["value"], r["p"]["value"], r["o"]["value"]) for r in results["results"]["bindings"]]
+
+        return data
+
+    def autocomplete_sparql(self,endpoint, query):
+        '''
+           Method for autocomplete sparql
+        '''
+        data = []
+
+        sparql = SPARQLWrapper(endpoint)
+
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+
+        data = [(r["s"]["value"], r["o"]["value"]) for r in results["results"]["bindings"]]
 
         return data
