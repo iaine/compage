34ad46301bc60163ccb5b50646c2564885b05e88
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-08-03 16:45:09 +0100

    Moved the sparql worker and removed tracing message

---
+++ static/workers/sparql.js
@@ -0,0 +1,17 @@
+/**
+*  Queries the endpoint to return the predicate weightings and ways of thinking about a term
+*/
+onmessage = function(e) {
+  var workerResult;
+  //set up the main call for the data to parse. 
+  function sparqlListener () {
+    workerResult = JSON.parse(this.responseText);
+    postMessage(workerResult);
+  }
+
+  var oReq = new XMLHttpRequest();
+  oReq.open("POST", "/sparql", true);
+  oReq.addEventListener("load", sparqlListener);
+  oReq.setRequestHeader("Content-type", "application/json");
+  oReq.send(JSON.stringify({'entity': e.data[0]}));
+}
