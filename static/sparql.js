d2c17bb1cd4a224d83be61a3d817569f95df7ae8
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-08-03 18:19:19 +0100

    Modved sparql

--- static/sparql.js
+++ static/sparql.js
@@ -3,7 +3,6 @@
 */
 onmessage = function(e) {
   var workerResult;
-  console.log('Message received from main script');
   //set up the main call for the data to parse. 
   function sparqlListener () {
     workerResult = JSON.parse(this.responseText);
@@ -15,7 +14,4 @@ onmessage = function(e) {
   oReq.addEventListener("load", sparqlListener);
   oReq.setRequestHeader("Content-type", "application/json");
   oReq.send(JSON.stringify({'entity': e.data[0]}));
-
-  /*console.log('Posting message back to main script');
-  postMessage(workerResult);*/
 }
