804db58d1922efe3de52a622afc1cf2228f43324
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-08-03 18:18:45 +0100

    Workset now a singleton module object

--- static/worksets.js
+++ static/worksets.js
@@ -1,30 +1,49 @@
 /**
-*  Workset object methods
+*  Workset object methods.
+*  Handles the updating of the object and its markup
+*
 */
 
-var workset = {
-  init: function () {
+var workset = function() {
+  function init () {
     return new Set();
-  },
+  }
 
   /**
   *  Adds an id to the Set of worksets
   */
-  addid: function(workset, worksetid) {
+  function addid(workset, worksetid) {
     workset.add(worksetid);
     return workset;
-  },
+  }
 
   /**
   *  Filter a set of ids into a new array
   */
-  filterid: function(work, worksetid) {
+  function filterid(work, worksetid) {
      for (var ids of worksetid) {
          console.log(ids);
          work.delete(ids);
      }
      return work;
   }
-}
 
-module.exports = workset;
+  /**
+  *  Method to handle the markup for the workset list
+  */
+  function markup(workids) {
+    html = "<ul>";
+    workids.forEach( 
+      function(y) { html += "<li id=\""+ y.id+"\">"+ y.value +"</li>"; }
+    );
+    html += "</ul>";
+    return html;
+  }
+
+  return {
+      init:init, 
+      addid: addid, 
+      filterid:filterid, 
+      markup: markup
+  }
+}()
