99f34f42b58f523cda7d9b0a3eb442e307f0a9b9
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-08-09 14:42:39 +0100

    Fixes to jaccard similarity

--- similarity.py
+++ similarity.py
@@ -4,7 +4,7 @@
 
 class Similarities():
 
-    def jaccard_similarity(lista, listb):
+    def jaccard_similarity(self, lista, listb):
         '''
             Calculate the Jaccard similarity for two sets
         '''
@@ -12,15 +12,15 @@ class Similarities():
             raise ValueError('Empty list received')
         seta = set(lista)
         setb = set(listb)
-        return seta.intersection(setb) / seta.union(setb)
+        return float(len(seta.intersection(setb))) / float(len(seta.union(setb)))
 
-    def square_rooted(t):
+    def square_rooted(self, t):
         if t > 0:
             return round(sqrt(t*t),3)
         else:
             return 0.001
 
-    def cosine_similarity(x, y):
+    def cosine_similarity(self, x, y):
         numerator = x * y
         denominator = square_rooted(x)*square_rooted(y)
         return round(numerator/float(denominator),3)
@@ -44,13 +44,13 @@ class Similarities():
         '''
         similarities = []
         start_graph = None
-        for graphs in graph_list:
-            start_graph = graphs
-            for graph in graph_list:
-                stats = self.pair_similarities_stats(start_graph, graph_list)
-                #graph_links = self.pair_similarities_list(start_graph, graph_list)
-                #unlinks = self.pair_similarities_dissimilar(graph_links, graph)
-                similarities.push({'name':start_graph, 'js': stats})
+        for name, graphvalue in graph_list.iteritems():
+            start_graph = name
+            start_graph_values = graphvalue
+            stats = self.pair_similarities_stats(start_graph,start_graph_values, graph_list)
+            #graph_links = self.pair_similarities_list(start_graph, graph_list)
+            #unlinks = self.pair_similarities_dissimilar(graph_links, graph)
+            similarities.append({'name':start_graph, 'js': stats})
 
         return similarities
                 
@@ -62,19 +62,16 @@ class Similarities():
         '''
         return list(filter(lambda x: (x[1], x[2]), graphlist))
  
-    def pair_similarities_stats(self, init_graph, graph_list):
+    def pair_similarities_stats(self,init_name, init_graph, graph_list):
         '''
            Method to get the Jaccard similarity for the graphs 
            Returns a list of tuples of (lista, listb, similarity_score)
         '''
-
-        _init = self.filter_graph(init_graph)
         similarities = []
-        for graph in graph_list:
-            if graph != init_graph:
-                _graph = self.filter_graph(graph)
-                jaccard = self.jaccard_similarity(_init, _graph)
-                similarities.push((init_graph[0][0], graph[0][0], jaccard))
+        for graphname, graphvalue in graph_list.iteritems():
+            if graphname != init_name:
+                jaccard = self.jaccard_similarity(init_graph, graphvalue)
+                similarities.append((init_name, graphname, jaccard))
 
         return similarities
 
@@ -85,7 +82,7 @@ class Similarities():
 
         link = []
         
-        for graph in graph_list:
+        for name, value in graph_list,iteritems():
             if start_graph[1] == graph[1] and start_graph[2] == graph[2]:
                 link.append(graph)
 
