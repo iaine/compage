9cbaa857a3950e8f9cccff02b33d6ba29fffe2ab
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-08-17 11:50:54 +0100

    Removed object from predicate/ws payload

--- static/workers/sparql.js
+++ static/workers/sparql.js
@@ -15,7 +15,7 @@ onmessage = function(e) {
     payload = JSON.stringify({'entity': e.data[0]});
   } else {
     url = '/predicates/workset'; 
-    payload = JSON.stringify({'pred': e.data[0], 'obj': e.data[1], 'ws': e.data[2]}); 
+    payload = JSON.stringify({'pred': e.data[0], 'ws': e.data[1]}); 
   }
   var oReq = new XMLHttpRequest();
   oReq.open("POST", url, true);
